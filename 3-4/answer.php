00220652f19178734ab91082d8ad22f4c9c96fb3
kappa-tt <62650895+kappa-tt@users.noreply.github.com>
2022-04-18 23:45:01 +0900

    課題提出
    ディレクトリ「3-4」の作成

---
+++ 3-4/answer.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title></title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <?php 
+  //[question.php]から送られてきた名前の変数、選択した回答、問題の答えの変数を作成
+  $Name = $_POST["my_name"];
+  $Answer1 = filter_input(INPUT_POST, "q1", FILTER_SANITIZE_STRING);
+  $Answer2 = filter_input(INPUT_POST, "q2", FILTER_SANITIZE_STRING);
+  $Answer3 = filter_input(INPUT_POST, "q3", FILTER_SANITIZE_STRING);
+  $corAns1 = $_POST["a1"];
+  $corAns2 = $_POST["a2"];
+  $corAns3 = $_POST["a3"];
+  
+  
+  //選択した回答と正解が一致していれば「正解！」、一致していなければ「残念・・・」と出力される処理を組んだ関数を作成する
+  function result($answer, $corAns) {
+    if ($answer === $corAns) {
+      return "正解！";
+    }else {
+      return "残念・・・";
+    }
+  }
+  
+  ?>
+  <p><!--POST通信で送られてきた名前を表示-->
+  <?php echo $Name; ?>さんの結果は・・・？</p>
+  <p>①の答え</p>
+  <!--作成した関数を呼び出して結果を表示-->
+  <?php echo result($Answer1, $corAns1); ?>
+  
+  <p>②の答え</p>
+  <!--作成した関数を呼び出して結果を表示-->
+  <?php echo result($Answer2, $corAns2); ?>
+  
+  <p>③の答え</p>
+  <!--作成した関数を呼び出して結果を表示-->
+  <?php echo result($Answer2, $corAns2); ?>
+  
+</body>
+</html>
+
---
+++ 3-4/question.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title></title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <?php
+  //POST送信で送られてきた名前を受け取って変数を作成
+  $my_name = $_POST["my_name"];
+  
+  //①画像を参考に問題文の選択肢の配列を作成してください。
+  $question1 = [80, 22, 20, 21];
+  $question2 = ["PHP", "Python", "JAVA", "HTML"];
+  $question3 = ["join", "select", "insert", "update"];
+  
+  //② ①で作成した、配列から正解の選択肢の変数を作成してください
+
+  $answer1 = 80;
+  $answer2 = "PHP";
+  $answer3 = "select";
+  
+  ?>
+  <p>お疲れ様です<!--POST通信で送られてきた名前を出力--><?php echo $my_name; ?>さん</p>
+
+  <!--フォームの作成 通信はPOST通信で-->
+  <form action="answer.php" method="post">
+    
+    <h2>①ネットワークのポート番号は何番？</h2>
+    <!--③ 問題のradioボタンを「foreach」を使って作成する-->
+    <?php foreach ($question1 as $value) { ?>
+      <input type="radio" name="q1" value="<?php echo $value; ?>"><?php echo $value; ?>
+    <?php } ?>
+    
+    <h2>②Webページを作成するための言語は？</h2>
+    <!--③ 問題のradioボタンを「foreach」を使って作成する-->
+    <?php foreach ($question2 as $value) { ?>
+      <input type="radio" name="q2" value="<?php echo $value; ?>"><?php echo $value; ?>
+    <?php } ?>
+    
+    
+    <h2>③MySQLで情報を取得するためのコマンドは？</h2>
+    <!--③ 問題のradioボタンを「foreach」を使って作成する-->
+    <?php foreach ($question3 as $value) { ?>
+      <input type="radio" name="q3" value="<?php echo $value; ?>"><?php echo $value; ?>
+    <?php } ?>
+    
+    <!--問題の正解の変数と名前の変数を[answer.php]に送る-->
+    <input type="hidden" name="a1" value="<?php echo $answer1; ?>">
+    <input type="hidden" name="a2" value="<?php echo $answer2; ?>">
+    <input type="hidden" name="a3" value="<?php echo $answer3; ?>">
+    <input type="hidden" name="my_name" value="<?php echo $my_name; ?>">
+
+    <br>
+    <input type="submit" value="回答する">
+    
+  </form>
+  
+</body>
+</html>
+
