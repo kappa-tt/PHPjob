ce766f218cef7e9d39d6d797197ff33378178fb5
kappa-tt <62650895+kappa-tt@users.noreply.github.com>
2022-04-24 23:26:52 +0900

    課題提出
    ディレクトリ「4-1」の作成

---
+++ 4-1/bugTest/bugTest/test2/dbconnect.php
@@ -0,0 +1,13 @@
+<?php
+// セッション開始
+session_start();
+// DBサーバのURL
+$db['host'] = "localhost";
+// ユーザー名
+$db['user'] = "root";
+// ユーザー名のパスワード
+// パスワードの修正
+$db['pass'] = "root";
+// データベース名
+// データベース名の修正
+$db['dbname'] = "YIGroupBlog";
